d48917aa1c8ed02ffbb394bb1c8452bf920ce74e
Sue Ceklosky <cekloskys@chc.edu>
2023-11-10 09:26:20 -0500

    Second version of collection-classes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,366 @@
 from node.node import *
 
 def main():
-    testInit()
+    # testInit()
+    # testGettersAndSetters()
+    # testAddNodeAfter()
+    # testRemoveNodeAfter()
+    # review()
+    # testListLength()
+    # testListSearch()
+    # testListPosition()
+    # testListCopy()
+    testListCopyWithTail()
+
+def testListCopyWithTail():
+    print("Testing List Copy With Tail")
+
+    # construct a node with data equal to S and link equal to None
+    # and assign its reference to source
+    source = node('S', None) # S
+
+    # construct a node with data equal to B and link equal to source
+    # and assign its reference to source
+    source = node('B', source) # B -> S
+
+    # construct a node with data equal to O and link equal to source
+    # and assign its reference to source
+    source = node('O', source) # O -> B -> S
+
+    # construct a node with data equal to S and link equal to source
+    # and assign its reference to source
+    source = node('J', source) # J -> O -> B -> S
+
+    copy = node.listCopyWithTail(source) 
+    # [J -> O -> B -> S, S]
+
+    print("Source contains", node.listPosition(source, 1).getData(),
+          node.listPosition(source, 2).getData(),
+          node.listPosition(source, 3).getData(),
+          node.listPosition(source, 4).getData())
+    
+    print("Copy head contains", node.listPosition(copy[0], 1).getData(),
+          node.listPosition(copy[0], 2).getData(),
+          node.listPosition(copy[0], 3).getData(),
+          node.listPosition(copy[0], 4).getData())
+    
+    print("Copy tail contains", node.listPosition(copy[1], 1).getData())
+
+def testListCopy():
+    print("Testing List Copy")
+
+    # construct a node with data equal to S and link equal to None
+    # and assign its reference to source
+    source = node('S', None) # S
+
+    # construct a node with data equal to B and link equal to source
+    # and assign its reference to source
+    source = node('B', source) # B -> S
+
+    # construct a node with data equal to O and link equal to source
+    # and assign its reference to source
+    source = node('O', source) # O -> B -> S
+
+    # construct a node with data equal to S and link equal to source
+    # and assign its reference to source
+    source = node('J', source) # J -> O -> B -> S
+
+    copy = node.listCopy(source) # J -> O -> B -> S, but at a different memory location
+
+    print("Source contains", node.listPosition(source, 1).getData(),
+          node.listPosition(source, 2).getData(),
+          node.listPosition(source, 3).getData(),
+          node.listPosition(source, 4).getData())
+    
+    print("Copy contains", node.listPosition(copy, 1).getData(),
+          node.listPosition(copy, 2).getData(),
+          node.listPosition(copy, 3).getData(),
+          node.listPosition(copy, 4).getData())
+
+def testListPosition():
+    print("Testing List Position")
+
+    # construct a node with data equal to S and link equal to None
+    # and assign its reference to head
+    head = node('S', None) # S
+
+    # construct a node with data equal to B and link equal to head
+    # and assign its reference to head
+    head = node('B', head) # B -> S
+
+    # construct a node with data equal to O and link equal to head
+    # and assign its reference to head
+    head = node('O', head) # O -> B -> S
+
+    # construct a node with data equal to S and link equal to head
+    # and assign its reference to head
+    head = node('J', head) # J -> O -> B -> S
+
+    print("First node contains data:", node.listPosition(head, 1).getData()) # J -> O -> B -> S
+    print("Second node contains data:", node.listPosition(head, 2).getData()) # O -> B -> S
+    print("Third node contains data:", node.listPosition(head, 3).getData()) # B -> S
+    print("Fourth node contains data:", node.listPosition(head, 4).getData()) # S
+
+    if (node.listPosition(head, 5) != None):
+        print("Fifth node contains data:", node.listPosition(head, 5).getData())
+    else:
+        print("There is no fifth node.")
+
+def testListSearch():
+    print("Testing List Search")
+
+    # construct a node with data equal to S and link equal to None
+    # and assign its reference to head
+    head = node('S', None) # S
+
+    # construct a node with data equal to B and link equal to head
+    # and assign its reference to head
+    head = node('B', head) # B -> S
+
+    # construct a node with data equal to O and link equal to head
+    # and assign its reference to head
+    head = node('O', head) # O -> B -> S
+
+    # construct a node with data equal to S and link equal to head
+    # and assign its reference to head
+    head = node('J', head) # J -> O -> B -> S
+
+    print("Head contains", node.listSearch(head, 'J').getData())
+    print("Head contains", node.listSearch(head, 'O').getData())
+    print("Head contains", node.listSearch(head, 'B').getData())
+    print("Head contains", node.listSearch(head, 'S').getData())
+
+    if (node.listSearch(head, 'Z') != None):
+        print("Head contains", node.listSearch(head, 'Z').getData())
+    else:
+        print("Head doesn't contain Z.")
+
+def testListLength():
+    print("Testing List Length")
+
+    # construct a node with data equal to S and link equal to None
+    # and assign its reference to head
+    head = node('S', None) # S
+
+    # construct a node with data equal to B and link equal to head
+    # and assign its reference to head
+    head = node('B', head) # B -> S
+
+    # construct a node with data equal to O and link equal to head
+    # and assign its reference to head
+    head = node('O', head) # O -> B -> S
+
+    # construct a node with data equal to S and link equal to head
+    # and assign its reference to head
+    head = node('J', head) # J -> O -> B -> S
+
+    print("Length of head is:", node.listLength(head))
+
+def review():
+    print("Review")
+
+    # Question 1
+    head = node('X', None)
+    head = node('X', head)
+    head = node('X', head)
+    head = node('X', head)
+
+    # Question 2
+    selection1 = head
+
+    # Question 3
+    selection1.addNodeAfter('O')
+
+    # Question 4
+    selection1 = selection1.getLink()
+    selection1 = selection1.getLink()
+
+    # Question 5
+    selection1.addNodeAfter('O')
+
+    # Question 6
+    selection1 = selection1.getLink()
+    selection1 = selection1.getLink()
+
+    # Question 7
+    selection1.addNodeAfter('O')
+
+    # Question 8
+    tail = head
+
+    # Question 9
+    tail = tail.getLink()
+    tail = tail.getLink()
+    tail = tail.getLink()
+    tail = tail.getLink()
+    tail = tail.getLink()
+    tail = tail.getLink()
+
+    # Question 10
+    selection2 = head
+
+    # Question 11
+    selection2 = selection2.getLink()
+    selection2 = selection2.getLink()
+
+    # Question 12
+    head.setData('A')
+    selection2.setData('A')
+    selection1.setData('A')
+    tail.setData('A')
+
+    # Question 13
+    head.removeNodeAfter()
+    selection1.removeNodeAfter()
+    selection2.removeNodeAfter()
+
+def testRemoveNodeAfter():
+    print("Testing Remove Node After")
+
+    # construct a node with data equal to S and link equal to None
+    # and assign its reference to head
+    head = node('S', None) # S
+
+    # construct a node with data equal to B and link equal to head
+    # and assign its reference to head
+    head = node('B', head) # B -> S
+
+    # construct a node with data equal to O and link equal to head
+    # and assign its reference to head
+    head = node('O', head) # O -> B -> S
+
+    # construct a node with data equal to S and link equal to head
+    # and assign its reference to head
+    head = node('J', head) # J -> O -> B -> S
+
+    print("The head node contains data:", head.getData())
+
+    # remove the node after the node head refers to (node that has data equal to O)
+    head.removeNodeAfter() # J -> B -> S
+
+    head = head.getLink() # B -> S
+
+    print("The head node contains data:", head.getData())
+
+    # remove the node after the node head refers to (node that has data equal to S)
+    head.removeNodeAfter() # B
+
+    print("The head node contains data:", head.getData())
+
+    """ # remove the node after the node head refers to (node that has data equal to S)
+    head.removeNodeAfter()  # this line of code will generate an AttributeError"""
+
+def testAddNodeAfter():
+    print("Testing Add Node After")
+
+    # construct a node with data equal to J and link equal to None
+    # and assign its reference to head
+    head = node('J', None) # J
+
+    print("The head node contains data:", head.getData())
+
+    # declare a new node named selection and make it refer
+    # to the same node as head
+    selection = head # J
+
+    print("The head node contains data:", head.getData())
+    print("The selection node contains data:", selection.getData())
+
+    # add a node with data equal to O after the node selection
+    # refers to
+    selection.addNodeAfter('O') # J -> O
+
+    # get selection's link and assign its reference back to
+    # selection
+    selection = selection.getLink() # O
+
+    print("The head node contains data:", head.getData())
+    print("The selection node contains data:", selection.getData())
+
+    # add a node with data equal to B after the node selection
+    # refers to
+    selection.addNodeAfter('B') # O -> B
+
+    # get selection's link and assign its reference back to
+    # selection
+    selection = selection.getLink() # B
+
+    print("The head node contains data:", head.getData())
+    print("The selection node contains data:", selection.getData())
+
+    # add a node with data equal to S after the node selection
+    # refers to
+    selection.addNodeAfter('S') # B -> S
+
+    # get selection's link and assign its reference back to
+    # selection
+    selection = selection.getLink() # S
+
+    print("The head node contains data:", head.getData())
+    print("The selection node contains data:", selection.getData())
+
+    # declare a new node named tail and make it refer to the same
+    # node as head
+    tail = head
+
+    # get tail's link and assign its reference to tail
+    tail = tail.getLink()
+    tail = tail.getLink()
+    tail = tail.getLink()
+
+    # add a new node with data equal to Z after the node tail
+    # refers to
+    tail.addNodeAfter('Z')
+
+    # get tail's link and assign its reference to tail
+    tail = tail.getLink()
+
+    print("The head node contains data:", head.getData())
+    print("The selection node contains data:", selection.getData())
+    print("The tail node contains data:", tail.getData())
+
+def testGettersAndSetters():
+    print("Testing Getters And Setters")
+
+    # construct a node with data equal to R and link equal to None
+    # and assign its reference to head
+    head = node('R', None) # R
+
+    print("The head node contains data:", head.getData())
+
+    head.setData('S') # S
+    print("The head node contains data:", head.getData())
+
+    head = node('B', head) # B -> S
+    print("The head node contains data:", head.getData())
+
+    head = node('O', head) # O -> B -> S
+    print("The head node contains data:", head.getData())
+
+    head = node('J', head) # J -> O -> B -> S
+    print("The head node contains data:", head.getData())
+
+    # get head's link and assign its reference back to head
+    head = head.getLink() # O -> B -> S
+    print("The head node contains data:", head.getData())
+
+    # get head's link and assign its reference back to head
+    head = head.getLink() # B -> S
+    print("The head node contains data:", head.getData())
+
+    # get head's link and assign its reference back to head
+    head = head.getLink() # S
+    print("The head node contains data:", head.getData())
+
+    # get head's link and assign its reference back to head
+    """ head = head.getLink() # None
+    print("The head node contains data:", head.getData()) """
+
+    print("The head node contains link:", head.getLink())
+
+    # set head's link to a new node
+    head.setLink(node('O', None)) # S -> O
+
 
 def testInit():
     print("Testing Node Init")
--- src/node/node.py
+++ src/node/node.py
@@ -47,3 +47,179 @@ class node:
             link (node): specified link
         """        
         self.__link = link
+
+    def addNodeAfter(self, element):
+        """Adds a new node containing a specified element value
+        at a selected position in the calling node.
+
+        Args:
+            element (_type_): specified element value
+        """        
+        self.__link = node(element, self.__link)
+
+    def removeNodeAfter(self):
+        """Removes a node from a selected position in the calling node.
+        """        
+        self.__link = self.__link.__link
+
+    @staticmethod
+    def listLength(head):
+        """Computes and returns the number of nodes in a specified node.
+
+        Args:
+            head (node): specified node
+
+        Returns:
+            int: number of nodes
+        """  
+        cursor = head   # cursor used to step through the specified node
+        length = 0      # used to count the nodes
+
+        # step through the nodes in the specified node as long as the
+        # cursor isn't null
+        while (cursor != None):
+            # increment length
+            length += 1
+
+            # move cursor to next node
+            cursor = cursor.getLink()
+
+        # return length
+        return length
+
+    @staticmethod
+    def listSearch(head, target):
+        """Searches for a specified target in a specified node.
+
+        Args:
+            head (node): specified node
+            target: specified target
+
+        Returns:
+            node: reference to node that contains specified target value
+            if specified target value is found, else None
+        """      
+        cursor = head   # cursor used to step through the specified node
+
+        # step through the nodes in the specified node as long as the
+        # cursor isn't None
+        while (cursor != None):
+            # check if the data value in the node cursor refers to is equal
+            # to the target
+            if (cursor.getData() == target):
+                # return cursor
+                return cursor
+
+            # move cursor to next node
+            cursor = cursor.getLink()
+
+        # return None
+        return None  
+    
+    @staticmethod
+    def listPosition(head, position: int):
+        """Searches for a node in a specified node based on a specified position.
+
+        Args:
+            head (node): specified node
+            position (int): specified position
+
+        Raises:
+            ValueError: indicates position is less than one
+
+        Returns:
+            node: reference to node at specified position if specified position
+            is found, else None
+        """       
+        cursor = head   # used to step through the specified node
+        i = 1           # used to count the nodes 
+
+        try:
+            # if position is less than 1, raise error
+            if (position < 1):
+                raise ValueError("Position may not be less than 1.")
+        except ValueError as e:
+            # display error and exit
+            exit(e)
+        else:
+            # move cursor forward the correct number of nodes
+            # as long as i is less than position and cursor isn't
+            # equal to None
+            # if cursor becomes None, that means the specified position
+            # was greater than the number of nodes in the specified node
+            while ((i < position) and (cursor != None)):
+                # move the cursor to the next node
+                cursor = cursor.getLink()
+                # increment counter variable
+                i += 1
+
+            # return cursor
+            return cursor
+        
+    @staticmethod
+    def listCopy(source):
+        """Make a copy of a specified node.
+
+        Args:
+            source (node): specified node
+
+        Returns:
+            node: reference to the head node in the copy
+        """      
+        # if specified source node is None, return None
+        if (source is None):
+            return None
+
+        # make copy head refer to a node that contains the same
+        # data value in the specified source node to be copied
+        copyHead = node(source.getData(), None)
+        # make copy tail refer to the same node as copy head
+        copyTail = copyHead
+
+        # keep looping through the specified source node to be copied
+        # until we reach the node that has a link of None
+        while (source.getLink() != None):
+            # advance to the next node in the specified source node to be copied
+            source = source.getLink()
+            # get the data value in the specified source nod and add it to the
+            # end of copy tail
+            copyTail.addNodeAfter(source.getData())
+            # advance copy tail to the next node
+            copyTail = copyTail.getLink()
+
+        # return copy head
+        return copyHead
+    
+    @staticmethod
+    def listCopyWithTail(source):
+        """Makes a copy of a specified node
+
+        Args:
+            source (node): specified node to be copied
+
+        Returns:
+            [node]: references to head and tail of the copy
+        """   
+        # if specified source node is None, return None
+        if (source is None):
+            return None
+
+        # make copy head refer to a node that contains the same
+        # data value in the specified source node to be copied
+        copyHead = node(source.getData(), None)
+        # make copy tail refer to the same node as copy head
+        copyTail = copyHead
+
+        # keep looping through the specified source node to be copied
+        # until we reach the node that has a link of None
+        while (source.getLink() != None):
+            # advance to the next node in the specified source node to be copied
+            source = source.getLink()
+            # get the data value in the specified source nod and add it to the
+            # end of copy tail
+            copyTail.addNodeAfter(source.getData())
+            # advance copy tail to the next node
+            copyTail = copyTail.getLink()
+
+        # return copy head and copy tail
+        return [copyHead, copyTail]     
